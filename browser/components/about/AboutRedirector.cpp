bd398a5fa445abe412da48ce0ccf63f9789d94ce
Dan Mosedale <dmose@mozilla.org>
2022-06-17 16:04:22 -0700

    WIP

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -106,6 +106,8 @@ static const RedirEntry kRedirMap[] = {
          nsIAboutModule::URI_CAN_LOAD_IN_PRIVILEGEDABOUT_PROCESS |
          nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
          nsIAboutModule::ALLOW_SCRIPT},
+    {"asrouter-devtools", "chrome://browser/content/asRouterDevtools.html",
+     ACTIVITY_STREAM_FLAGS}, // XXX maybe don't need the UNSANITIZED and can switch to a-w flags?
     {"pocket-saved", "chrome://pocket/content/panels/saved.html",
      nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
          nsIAboutModule::URI_CAN_LOAD_IN_CHILD | nsIAboutModule::ALLOW_SCRIPT |
