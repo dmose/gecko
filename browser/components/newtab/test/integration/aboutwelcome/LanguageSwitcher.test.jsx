dc2ba6b375fdf4ae654f0d0001753267d522f68c
Dan Mosedale <dmose@mozilla.org>
2022-07-12 10:05:21 -0700

    Add LanguageSwitcher.test.jsx

---
+++ browser/components/newtab/test/integration/aboutwelcome/LanguageSwitcher.test.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { LanguageSwitcher } from "content-src/aboutwelcome/components/LanguageSwitcher.jsx"
+import { render, screen } from "@testing-library/react";
+
+describe("LanguageSwitcher")
